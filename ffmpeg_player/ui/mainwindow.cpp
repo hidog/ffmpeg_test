3aab1f8c8ec40b1cd664b05ce82fd8be24370567
hidog <hidog.gao@gmail.com>
2022-01-18 20:18:01 +0800

    add ip, port default value.

--- ffmpeg_player/ui/mainwindow.cpp
+++ ffmpeg_player/ui/mainwindow.cpp
@@ -156,6 +156,11 @@ void    MainWindow::start_connect_slot()
     std::string     ip      =   ui->ipLEdit->text().toStdString();
     std::string     port    =   ui->portLEdit->text().toStdString();
 
+    if( ip.empty() == true )
+        ip      =   "127.0.0.1";
+    if( port.empty() == true )
+        port    =   "1234";
+
     worker->set_ip( ip );
     worker->set_port( port );
    
