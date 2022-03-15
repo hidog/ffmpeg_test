9a9fab003948fae24a256ec8df5c4d3f1ab4f0d6
hidog <hidog.gao@gmail.com>
2022-03-15 15:51:01 +0800

    add support subtitle format.

--- ffmpeg_player/src/worker.cpp
+++ ffmpeg_player/src/worker.cpp
@@ -55,8 +55,9 @@ QStringList Worker::get_subtitle_files( std::string filename )
 
     //
     QStringList     filter;
-    filter.push_back("*.ass");
-    filter.push_back("*.ssa");
+    filter << "*.ass" << "*.ssa" << "*.srt" << "*.sub";
+    //filter.push_back("*.ass");
+    //filter.push_back("*.ssa");
     //filter.push_back("*.sub");
 
     //
