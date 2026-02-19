2b6fbad31bd25caee04a52337ad075c875ba04cd
hidog <hidog.gao@gmail.com>
2026-02-19 17:07:00 +0800

    revert disable assert.

--- src/tool.h
+++ src/tool.h
@@ -41,11 +41,9 @@ enum class LOG
             printf("[ERR] [%s] [%d] ", __FILE__, __LINE__); \
 		printf(__VA_ARGS__); \
 		printf("\n"); \
+        if( TAG == LOG::L_ERROR ) \
+            assert(0); \
 	}
-
-
-        //if( TAG == LOG::L_ERROR ) \
-        //    assert(0); 
 
 
 
